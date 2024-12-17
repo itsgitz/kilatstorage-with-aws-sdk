af121072881602a797a38568ce0487ce4dc7c13a
itsgitz <anggit.ginanjar.dev@gmail.com>
2024-12-17 12:18:52 +0700

    fix: file datatype must filestream for upload

--- upload.js
+++ upload.js
@@ -1,5 +1,6 @@
 import 'dotenv/config'
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
+import {createReadStream} from 'fs'
 
 if (!process.env.KILAT_STORAGE_ACCESS_KEY_ID) {
   throw Error('KilatStorage access key is required!')
@@ -18,28 +19,35 @@ if (!process.env.KILAT_STORAGE_BUCKETNAME) {
 }
 
 const upload = async () => {
-  // KilatStorage can be accessed using AWS SDK v3 for Node.js
-  // - Select the region us-east-1 as default region
-  // - We use different endpoint from AWS, thus use https://s3-id-jkt-1.kilatstorage.id
-  const client = new S3Client({
-    region: 'us-east-1',
-    credentials: {
-      accessKeyId: process.env.KILAT_STORAGE_ACCESS_KEY_ID,
-      secretAccessKey: process.env.KILAT_STORAGE_SECRET_ACCESS_KEY
-    },
-    endpoint: process.env.KILAT_STORAGE_ENDPOINT
-  })
+  try {
+    const localFilePath = './img/icon-cloudkilat-white@2x.png' 
+    const fileStream = createReadStream(localFilePath)
 
-  // Upload image from ./img directory
-  const command = new PutObjectCommand({
-    ACL: 'public-read',
-    Bucket: process.env.KILAT_STORAGE_BUCKETNAME,
-    Key: 'icon-cloudkilat-white@2x.png',
-    Body: './img/icon-cloudkilat-white@2x.png'
-  })
-  console.log('Uploading object to', process.env.KILAT_STORAGE_BUCKETNAME)
-  const response = await client.send(command)
-  console.log(response)
+    // KilatStorage can be accessed using AWS SDK v3 for Node.js
+    // - Select the region us-east-1 as default region
+    // - We use different endpoint from AWS, thus use https://s3-id-jkt-1.kilatstorage.id
+    const client = new S3Client({
+      region: 'us-east-1',
+      credentials: {
+        accessKeyId: process.env.KILAT_STORAGE_ACCESS_KEY_ID,
+        secretAccessKey: process.env.KILAT_STORAGE_SECRET_ACCESS_KEY
+      },
+      endpoint: process.env.KILAT_STORAGE_ENDPOINT
+    })
+
+    // Upload image from ./img directory
+    const command = new PutObjectCommand({
+      ACL: 'public-read',
+      Bucket: process.env.KILAT_STORAGE_BUCKETNAME,
+      Key: 'icon-cloudkilat-white@2x.png',
+      Body: fileStream
+    })
+    console.log('Uploading object to', process.env.KILAT_STORAGE_BUCKETNAME)
+    const response = await client.send(command)
+    console.log(response)
+  } catch (e) {
+    console.log(e)
+  } 
 }
 
 upload()
